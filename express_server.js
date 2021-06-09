339c3f7e7169273adb0def28902adf023a2738b5
dextergb <dextergb@rogers.com>
2021-06-09 20:02:24 +0000

    Created a users Object.

--- express_server.js
+++ express_server.js
@@ -16,6 +16,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 /*
 * The order of route definitions matters!
 * The GET /urls/new route needs to be defined before the GET /urls/:id route.
