0288ea330746d43457491b8570d64fdc677564c4
dextergb <dextergb@rogers.com>
2021-06-09 19:55:36 +0000

    Create Registration Page: New template form for register. Create a GET register endpoint for new template.

--- express_server.js
+++ express_server.js
@@ -33,6 +33,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render("register", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
